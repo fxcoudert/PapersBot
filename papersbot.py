328496525897faa5a3e21e338532f5aabef88d9e
FX Coudert <fxcoudert@gmail.com>
2018-07-29 10:22:39 +0900

    Style fixes

--- papersbot.py
+++ papersbot.py
@@ -9,9 +9,19 @@
 # e-mail:   fxcoudert@gmail.com
 #
 
-import bs4, feedparser, tweepy
-import imghdr, json, os, re, sys, tempfile, time, urllib, yaml
+import imghdr
+import json
+import os
+import re
+import sys
+import tempfile
+import time
+import urllib
+import yaml
 
+import bs4
+import feedparser
+import tweepy
 
 
 # This is the regular expression that selects the papers of interest
@@ -28,7 +38,6 @@ regex = re.compile(r"""
   """, re.IGNORECASE | re.VERBOSE)
 
 
-
 # We select entries based on title or summary (abstract, for some feeds)
 def entryMatches(entry):
   if regex.search(entry.title):
@@ -39,20 +48,21 @@ def entryMatches(entry):
     return False
 
 
-acsTwitter = { "acsaem": "acs_aem", "acsami": "acs_ami", "acsanm": "acs_anm", "acscatal": "ACSCatalysis",
+acsTwitter = {"acsaem": "acs_aem", "acsami": "acs_ami", "acsanm": "acs_anm", "acscatal": "ACSCatalysis",
   "acscentsci": "ACSCentSci", "acsenergylett": "ACSEnergyLett", "acsnano": "acsnano", "acssuschemeng": "ACSSustainable",
   "acs.chemrev": "ACSChemRev", "acs.chemmater": "ChemMater", "acs.cgd": "CGD_ACS", "acs.est": "EnvSciTech",
   "acs.inorgchem": "InorgChem", "jacs": "J_A_C_S", "acs.jcim": "JCIM_ACS", "acs.jpcb": "JPhysChem", "acs.jpcc": "JPhysChem",
-  "acs.jpclett": "JPhysChem", "acs.langmuir": "ACS_Langmuir", "acs.nanolett": "NanoLetters" }
-rscTwitter = { "CY": "CatalysisSciTec", "CC": "ChemCommun", "SC": "ChemicalScience", "CS": "ChemSocRev", "CE": "CrystEngComm",
+  "acs.jpclett": "JPhysChem", "acs.langmuir": "ACS_Langmuir", "acs.nanolett": "NanoLetters"}
+rscTwitter = {"CY": "CatalysisSciTec", "CC": "ChemCommun", "SC": "ChemicalScience", "CS": "ChemSocRev", "CE": "CrystEngComm",
   "DT": "DaltonTrans", "EE": "EES_journal", "EN": "EnvSciRSC", "FD": "Faraday_D", "GC": "green_rsc", "TA": "JMaterChem",
-  "TB": "JMaterChem", "TC": "JMaterChem", "NR": "Nanoscale_RSC", "CP": "PCCP", "SM": "SoftMatter" }
-natureTwitter = { "s41563": "NatureMaterials", "s41557": "NatureChemistry", "s42004": "CommsChem", "s41467": "NatureComms",
+  "TB": "JMaterChem", "TC": "JMaterChem", "NR": "Nanoscale_RSC", "CP": "PCCP", "SM": "SoftMatter"}
+natureTwitter = {"s41563": "NatureMaterials", "s41557": "NatureChemistry", "s42004": "CommsChem", "s41467": "NatureComms",
   "s41929": "NatureCatalysis", "s41560": "NatureEnergyJnl", "s41565": "NatureNano", "s41567": "NaturePhysics",
-  "s42005": "CommsPhys", "s41570": "NatRevChem", "s41578": "NatRevMater" }
-wileyTwitter = { "adma": "AdvMater", "adfm": "AdvFunctMater", "anie": "angew_chem", "chem": "ChemEurJ", "asia": "ChemAsianJ",
-  "cplu": "ChemPlusChem", "cphc": "ChemPhysChem", "slct": "ChemistrySelect" }
-apsTwitter = { "PhysRevLett": "PhysRevLett", "PhysRevX": "PhysRevX", "PhysRevB": "PhysRevB", "PhysRevMaterials": "PhysRevMater" }
+  "s42005": "CommsPhys", "s41570": "NatRevChem", "s41578": "NatRevMater"}
+wileyTwitter = {"adma": "AdvMater", "adfm": "AdvFunctMater", "anie": "angew_chem", "chem": "ChemEurJ", "asia": "ChemAsianJ",
+  "cplu": "ChemPlusChem", "cphc": "ChemPhysChem", "slct": "ChemistrySelect"}
+apsTwitter = {"PhysRevLett": "PhysRevLett", "PhysRevX": "PhysRevX", "PhysRevB": "PhysRevB", "PhysRevMaterials": "PhysRevMater"}
+
 
 # From a given URL, figure out the corresponding journal Twitter handle
 # (This will work well for ACS, RSC, and Wiley Chemistry)
@@ -105,7 +115,8 @@ def htmlToText(s):
 
 
 def downloadImage(url):
-  if not url: return None
+  if not url:
+    return None
 
   try:
     img, _ = urllib.request.urlretrieve(url)
@@ -167,7 +178,7 @@ def cleanText(s):
   # Some feeds have LF characeters
   s = s.replace("\x0A", "")
   # Remove multiple spaces, leading and trailing space
-  return re.sub("\s\s+" , " ", s).strip()
+  return re.sub("\\s\\s+", " ", s).strip()
 
 
 # Read list of feed items already posted
@@ -184,7 +195,7 @@ class PapersBot:
   n_seen = 0
   n_tweeted = 0
 
-  def __init__(self, doTweet = True):
+  def __init__(self, doTweet=True):
     self.feeds = readFeedsList()
     self.posted = readPosted()
 
@@ -219,7 +230,7 @@ class PapersBot:
     # Start-up banner
     print(f"This is PapersBot running at {time.strftime('%Y-%m-%d %H:%M:%S %Z')}")
     if self.api:
-      last = self.api.user_timeline(count = 1)[0].created_at
+      last = self.api.user_timeline(count=1)[0].created_at
       print(f"Last tweet was posted at {last} (UTC)")
     print(f"Feed list has {len(self.feeds)} feeds\n")
 
@@ -278,8 +289,9 @@ class PapersBot:
         if entryMatches(entry):
           self.n_seen += 1
           # If no ID provided, use the link as ID
-          if not "id" in entry: entry.id = entry.link
-          if not entry.id in self.posted:
+          if "id" not in entry:
+            entry.id = entry.link
+          if entry.id not in self.posted:
             self.sendTweet(entry)
             # Bail out if we have reached max number of tweets
             if self.throttle > 0 and self.n_tweeted >= self.throttle:
@@ -294,13 +306,13 @@ class PapersBot:
 
 
   # Print out the n top tweets (most liked and RT'ed)
-  def printTopTweets(self, count = 5):
+  def printTopTweets(self, count=5):
     tweets = self.api.user_timeline(count=200)
     oldest = tweets[-1].created_at
     print(f"Top {count} recent tweets, by number of RT and likes, since {oldest}:\n")
 
-    tweets = [ (t.retweet_count + t.favorite_count, t.id, t) for t in tweets ]
-    tweets.sort(reverse = True)
+    tweets = [(t.retweet_count + t.favorite_count, t.id, t) for t in tweets]
+    tweets.sort(reverse=True)
     for _, _, t in tweets[0:count]:
       url = f"https://twitter.com/{t.user.screen_name}/status/{t.id}"
       print(f"{t.retweet_count} RT {t.favorite_count} likes: {url}")
@@ -308,17 +320,16 @@ class PapersBot:
       print(f"    {t.text}\n")
 
 
-
 def main():
   # Make sure all options are correctly typed
   options_allowed = ["--do-not-tweet", "--top-tweets"]
   for arg in sys.argv[1:]:
-    if not arg in options_allowed:
+    if arg not in options_allowed:
       print(f"Unknown option: {arg}")
       sys.exit(1)
 
   # Initialize our bot
-  doTweet = not "--do-not-tweet" in sys.argv
+  doTweet = "--do-not-tweet" not in sys.argv
   bot = PapersBot(doTweet)
 
   # We can print top tweets
@@ -328,7 +339,6 @@ def main():
 
   bot.run()
   bot.printStats()
-
 
 
 if __name__ == '__main__':
